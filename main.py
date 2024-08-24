495868613d8780a394dd3369da33ca85b2fc222e
Cagri01 <dalci22@itu.edu.tr>
2024-08-25 02:48:28 +0300

    add connection string

--- main.py
+++ main.py
@@ -3,24 +3,26 @@ import json
 from process_message import process_message
 from logger import log_incoming_message
 from pymavlink_helper import PyMavlinkHelper
+from heartbeat_processor import HeartbeatProcessor
+import argparse
 
 
-TOPIC_LIST = ["drone/1", "drone/2", "drone/3"]
 LOG_PATH = "logs/"
-PIXHAWK_CONNECTION_STRING = "serial:/dev/ttyAMA0:115200"
+PIXHAWK_CONNECTION_STRING = "serial:/dev/serial0:57600"
 
 
-def start_client():
+def start_client(client_id):
     helper = PyMavlinkHelper(PIXHAWK_CONNECTION_STRING)
+    heartbeat_processor = HeartbeatProcessor(die_time=10)
     # MQTT Configuration
     BROKER = "192.168.1.127"
     PORT = 1883
-    CLIENT_ID = "CLIENT1"
+    CLIENT_ID = "CLIENT_" + str(client_id)
 
     def on_message(client: mqtt.Client, userdata, message: mqtt.MQTTMessage) -> None:
         json_data = json.loads(message.payload.decode())
         log_incoming_message(json_data, LOG_PATH)
-        process_message(json_data, client, helper)
+        process_message(json_data, client, helper, heartbeat_processor, client_id)
 
     def on_connect(client: mqtt.Client, userdata, flags, rc) -> None:
         if rc == 0:
@@ -40,4 +42,10 @@ def start_client():
 
 
 if __name__ == "__main__":
-    start_client()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "client_id", type=int, help="The unique client ID for the MQTT client (0, 1, 2)"
+    )
+    args = parser.parse_args()
+
+    start_client(args.client_id)
