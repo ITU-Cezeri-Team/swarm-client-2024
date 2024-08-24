585c2bde71d17b60b41905f157efb253826109d4
Cagri01 <dalci22@itu.edu.tr>
2024-08-25 02:47:51 +0300

    update heartbeat

--- heartbeat.py
+++ heartbeat.py
@@ -5,8 +5,9 @@ import json
 
 
 def send_heartbeat(
-    heartbeat_interval,
-    topic,
+    heartbeat_interval: int,
+    client: mqtt.Client,
+    topic: str,
 ):
     msg = {
         "msg_type": "heartbeat",
@@ -14,10 +15,12 @@ def send_heartbeat(
     }
     # send a heartbeat every heartbeat_interval seconds in an infinite loop
     while True:
-        mqtt.Client.publish(topic, json.dumps(msg))
+        client.publish(topic, json.dumps(msg))
 
         time.sleep(heartbeat_interval)
 
 
-def start_heartbeat(client: mqtt.Client, heartbeat_interval: int):
-    threading.Thread(target=send_heartbeat, args=(heartbeat_interval,)).start()
+def start_heartbeat(client: mqtt.Client, heartbeat_interval: int, topic: str):
+    threading.Thread(
+        target=send_heartbeat, args=(heartbeat_interval, client, topic)
+    ).start()
