5ef39eb22b7d98f702c106ac0312356d4b317a6d
Cagri01 <dalci22@itu.edu.tr>
2024-08-25 00:26:23 +0300

    add logger

--- logger.py
+++ logger.py
@@ -1,27 +1,56 @@
-# def log_message(message: str, folder_path: str) -> None:
-#     """
-#     Logs a message to a file.
+import os
+import datetime
 
-#     Parameters
-#     ----------
-#         message (dict):
-#             The type of the ROS message.
-#         message (str):
-#             The content of the ROS message.
-#         time (str):
-#             The timestamp of the ROS message.
-#         filepath (str):
-#             The path to the log file.
-#     """
-#     file_path = os.path.join(folder_path, datetime.date.today().strftime("%Y-%m-%d") + ".txt")
 
-#     folder = os.path.dirname(file_path)
-#     if not os.path.exists(folder):
-#         os.makedirs(folder)
+def log_incoming_message(message: dict, folder_path: str) -> None:
+    """
+    Logs an incoming message to a file.
 
-#     if not os.path.exists(file_path):
-#         open(file_path, 'w').close()
+    Parameters
+    ----------
+        message (dict):
+            The message as dictionary.
+        filepath (str):
+            The path to the log file.
+    """
+    file_path = os.path.join(
+        folder_path, "INCOMING", datetime.date.today().strftime("%Y-%m-%d") + ".txt"
+    )
 
-#     time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-#     with open(file_path, 'a') as file:
-#         file.write(f"Time: {time} | {message_type} | Channel: {channel} | Message: {message}\n")
+    folder = os.path.dirname(file_path)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    if not os.path.exists(file_path):
+        open(file_path, "w").close()
+
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    with open(file_path, "a") as file:
+        file.write(f"Time: {time} | Message: {message}\n")
+
+
+def log_outgoing_message(message: dict, folder_path: str) -> None:
+    """
+    Logs an outgoing message to a file.
+
+    Parameters
+    ----------
+        message (dict):
+            The message as dictionary.
+        filepath (str):
+            The path to the log file.
+    """
+    file_path = os.path.join(
+        folder_path, "OUTGOING", datetime.date.today().strftime("%Y-%m-%d") + ".txt"
+    )
+
+    folder = os.path.dirname(file_path)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    if not os.path.exists(file_path):
+        open(file_path, "w").close()
+
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    with open(file_path, "a") as file:
+        file.write(f"Time: {time} | Message: {message}\n")
